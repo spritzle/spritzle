bef269a9deee3f2a1ee4d6942ceaf25fc7daebcf
Damien Churchill <damoxc@gmail.com>
2011-11-25 01:27:08 +0000

    stub code layout

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#
+# setup.py
+#
+# Copyright (C) 2011 Andrew Resch <andrewresch@gmail.com>
+#               2011 Damien Churchill <damoxc@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.    If not, write to:
+#   The Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor
+#   Boston, MA    02110-1301, USA.
+#
+
+from setuptools import setup
+
+setup (
+    name         = 'spritzle',
+    version      = '0.1',
+    fullname     = 'Spritzle Bittorrent Client',
+    description  = 'Lightweight Bitorrent Client'
+    author       = 'Andrew Resch, Damien Churchill',
+    author_email = 'andrewresch@gmail.com, damoxc@gmail.com',
+    keywords     = 'torrent bittorrent p2p fileshare filesharing',
+    url          = 'http://github.com/damoxc/spritzle',
+    license      = 'GPLv3',
+    packages     = ['spritzle'],
+    entry_points = {
+        'console_scripts': [
+            'spritzled =  spritzle.core:main'
+        ]
+    }
+)
