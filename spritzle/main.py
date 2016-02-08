14404869cfd14e37e1ce63598c7970d451b1b1c3
Andrew Resch <andrewresch@gmail.com>
2016-02-07 18:23:48 -0800

    Remove run_app as its now included in aiohttp 0.21.0

--- spritzle/main.py
+++ spritzle/main.py
@@ -39,43 +39,6 @@ from spritzle.core import core
 app = aiohttp.web.Application()
 
 
-def run_app(app, *, host='0.0.0.0', port=None,
-            shutdown_timeout=60.0, ssl_context=None,
-            print=print):  # pragma: no cover
-    """Run an app locally
-
-    TODO: Remove this when aiohttp release has it.
-    """
-    if port is None:
-        if not ssl_context:
-            port = 8080
-        else:
-            port = 8443
-
-    loop = app.loop
-
-    handler = app.make_handler()
-    srv = loop.run_until_complete(loop.create_server(handler, host, port,
-                                                     ssl=ssl_context))
-
-    scheme = 'https' if ssl_context else 'http'
-    prompt = '127.0.0.1' if host == '0.0.0.0' else host
-    print("======== Running on {scheme}://{prompt}:{port}/ ========\n"
-          "(Press CTRL+C to quit)".format(
-              scheme=scheme, prompt=prompt, port=port))
-
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:  # pragma: no branch
-        pass
-    finally:
-        srv.close()
-        loop.run_until_complete(srv.wait_closed())
-        loop.run_until_complete(handler.finish_connections(shutdown_timeout))
-        loop.run_until_complete(app.finish())
-    loop.close()
-
-
 class Main(object):
 
     def __init__(self, port, debug=False, config_dir=None):
@@ -125,7 +88,7 @@ class Main(object):
         for s in (signal.SIGINT, signal.SIGTERM):
             self.loop.add_signal_handler(s, functools.partial(self.stop))
 
-        run_app(app)
+        aiohttp.web.run_app(app)
 
 
 def bootstrap(config_dir=None):
