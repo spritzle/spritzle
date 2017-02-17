a5edc3713202e7d34c01132c1c566f18346ca8d9
Andrew Resch <andrewresch@gmail.com>
2017-02-16 16:53:59 -0800

    Revert saving config on exit

--- spritzle/core.py
+++ spritzle/core.py
@@ -66,7 +66,6 @@ class Core(object):
 
     def stop(self):
         self.alert.stop()
-        self.config.save()
 
     def on_session_stats_alert(self, alert):
         self.session_stats_future.set_result(alert.values)
