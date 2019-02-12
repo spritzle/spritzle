889d86dade735dc6a30b365b0ba73bf81711bceb
Andrew Resch <andrewresch@gmail.com>
2019-02-11 18:50:38 -0800

    Fix indentation

--- spritzle/daemon/resource/auth.py
+++ spritzle/daemon/resource/auth.py
@@ -59,7 +59,7 @@ async def auth_middleware(request, handler):
 
     peername = request.transport.get_extra_info('peername')
     if peername and peername[0] in config['auth_allow_hosts']:
-            return await handler(request)
+        return await handler(request)
 
     if request.rel_url.path == '/auth':
         return await handler(request)
